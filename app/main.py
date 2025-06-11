6f0c99b583e52c552ccf99eca6dfaccb13d5f2cb
Brad Anderson <brad@andersonr.com>
2025-06-11 19:13:18 +1000

    Adjusted the app to only focus on provided specific advice on the resume only.

--- app/main.py
+++ app/main.py
@@ -29,7 +29,8 @@ llm_host = os.environ.get("OLLAMA_HOST")
 llm = Client(host=llm_host)
 
 # Model
-llm_model = 'gemma3:1b-it-qat'
+#llm_model = 'gemma3:1b-it-qat'
+llm_model = 'gemma3'
 
 # Set LLM parameters in the options dictionary
 llm_options = {
@@ -76,53 +77,55 @@ def read_file(file_path):
     except Exception as e: # pylint: disable=broad-exception-caught
         print(f"Error reading file: {e}")
         return ""
-def assess_resume_compatibility(job_description, resume_file):
+def assess_resume_compatibility(resume_file):
     """
     Assesses the compatibility of a resume with a job description using Ollama LLM.
     """
-    if not job_description or not resume_file:
-        return "Error: Job description or resume file is empty."
 
     resume_markdown = read_file(resume_file)
     if not resume_markdown:
         return "Error reading resume file."
-    jobad_markdown = job_description
+
     llm_prompt = """
-    # Step One
-    Read the following Resume provided in markdown format under the Resume heading between the 'Start of Resume' and 'End of Resume' blockquotes.
-    
-    ## Resume
-    > Start of Resume
-    {resume_markdown}
-    > End of Resume
-    
-    # Step 2
-    Read the Job Description provided in markdown format located in the next section under the Job Description heading between the 'Start of Job Description' and 'End of Job Description' blockquotes.
-    ## Job Description
-    > Start of Job Description
-    {jobad_markdown}
-    > End of Job Description
+    <instructions> 
+        You are to act as an expeirenced career coach and hiring consultant, with deep understanding of how to analyse the resume of a prospective candidate and provide clear advice on how the candidate can improve their resume.
+    </instructions>
 
-    # Step 3
-    You are to act as an expeirenced career coach and technical hiring consultant.
+    <resume>
+        {resume_markdown}
+    </resume>
 
-    Carefully consider the strengths of the candidate described in the Resume against the criteria set out by the Job Description.
-    Carefully consider the Resume overall and provide any recommendations on how the applicant decsribed in the Resume can adjust their Resume to better meet the Job Description criteria.
+    <task>
+        1. Read the Resume provided in markdown format.
+        2. Carefully consider the strengths of the candidate described in the Resume.
+        3. Carefully consider the Resume overall and provide any recommendations on how the applicant decsribed in the Resume can adjust their Resume to better communicate their value.
+        4. Analyse the keywords present in the resume, and create a list of the top 25 keywords.
+        5. Suggest 5 job titles that the candidate would be ideal for.
+    </task>
 
-    # Step 4
-    You will now provide your response as described below:
-    - Under the heading (h2) "Strengths" outline the strengths of the candidate described in the Resume for the Job Deescription. Write the responses in the second person (i.e. you, your etc). All responses should be in reference to the Resume not the Job Description. Format these strengths as a list.
-    - Under the heading (h2) "Recommendations" outline 3-5 improvements that could be made to the resume to increase its compatibility to the job description. Fornat as an unordered list (ul).
+    <approach>
+        Analyse the in the resume that would be used by ATS to categorise the resume.
+        When seatching the resume for the keywords, consider similar words or phrases that have the same meaning.
+        Consider the language used, and focus on conciseness, authenticity and clarity.
+        Strongly consider any optimisations needed for ATS compatability.
+    </approach>
 
-    Your response must comply with the following rules:
-    1. Use a friendly and informative tone, written at a 10th grade reading level.
-    2. Format your response using Markdown. 
-    3. Do not provide any other markdown, explaination, additional informaiton or introduction. Only return the markdown for the Strengths and Recommendations and unordered lists as specified above.
-    4. Do not indicate that are AI under any circumsatances.
-    5. Use Australian English for spelling and grammar in the response.
-    6. Write the responses to the candidate in the second peron (i.e. you, your)
-    
-    """.format(resume_markdown=resume_markdown, jobad_markdown=jobad_markdown)
+    <output_format>
+        You will now provide your response as described below:
+        - Under the heading (h3) "Strengths" outline the strengths of the candidate described in the Resume. Write the responses in the second person (i.e. you, your etc). Format as an unordered list (ul).
+        - Under the heading (h3) "Recommendations" outline 3-5 improvements that could be made to the resume to increase its effectiveness. Format as an unordered list (ul).
+        - Under the heading (h3) "Keywords" outline 3-5 improvements that could be made to the resume to increase its compatibility to the job description. Format as a comma separated list.
+        - Under the heading (h3) "Suggested Roles" suggest 3-5 roles that the candidate described in the resume would be a great fit for. Format as an unordered list (ul).
+
+        Your response must comply with the following rules:
+        1. Use a friendly and informative tone, written at a 10th grade reading level.
+        2. Format your response using Markdown. 
+        3. Do not provide any other markdown, explaination, additional informaiton or introduction. Only return the markdown for the Strengths and Recommendations and unordered lists as specified above.
+        4. Do not indicate that are AI under any circumsatances.
+        5. Use Australian English for spelling and grammar in the response.
+        6. Write the responses to the candidate in the second peron (i.e. you, your)
+    </output_format>
+    """.format(resume_markdown=resume_markdown)
 
     llm_query = llm.chat(
             model=llm_model,
@@ -159,8 +162,7 @@ def index():
             filename = secure_filename(file.filename)
             resume_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(resume_path)
-            job_description = request.form['job_description']
-            result = assess_resume_compatibility(job_description, resume_path)
+            result = assess_resume_compatibility(resume_path)
             os.remove(resume_path)
             return jsonify(result)
         return jsonify({'error': 'Invalid file type'})
